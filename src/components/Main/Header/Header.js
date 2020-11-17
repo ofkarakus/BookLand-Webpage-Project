17c4c67818cb13bd05bd494e5e454545b4a92d27
ofkarakus <omerfarukkarakus01@gmail.com>
2020-11-17 21:03:55 +0000

    feat: add header

---
+++ src/components/Main/Header/Header.js
@@ -0,0 +1,28 @@
+import "./Header.style.scss";
+import book from "../../../assets/book.png";
+import loupe from "../../../assets/loupe.png";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={book} alt="logo_image" className="header__logo__img" />
+        <p className="header__logo__name">BookLand</p>
+      </div>
+      <div className="header__searchbar">
+        <img
+          src={loupe}
+          alt="search_icon"
+          className="header__searchbar__icon"
+        />
+        <input
+          type="text"
+          className="header__searchbar__input"
+          placeholder="Which book are you looking for?"
+        />
+        <button className="header__searchbar__btn">Search</button>
+      </div>
+      <div className="header__login">LOGIN</div>
+    </div>
+  );
+};
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import { Header } from "./";
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Main;
